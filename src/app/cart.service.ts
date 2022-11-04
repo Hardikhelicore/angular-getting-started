a11428187fa1741e78a1ed87f4239cb62ad880d1
Hardikhelicore <91306611+Hardikhelicore@users.noreply.github.com>
2022-11-04 11:31:36 +0530

    Retriving Data Using HttpClient

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,15 @@
 import { Product } from './products';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   items: Product[] = [];
 
@@ -19,5 +24,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  // Api Call
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
